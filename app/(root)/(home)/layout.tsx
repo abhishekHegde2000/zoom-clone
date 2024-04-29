4bacc773726827f16b453d95a7476c2ba2a13d6c
Abhishek Hegde <ahegde2021@gmail.com>
2024-04-29 14:46:35 +0530

    sidebar done

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,8 +1,8 @@
 import { Metadata } from "next";
 import { ReactNode } from "react";
 
-// import Navbar from "@/components/Navbar";
-// import Sidebar from "@/components/Sidebar";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 
 export const metadata: Metadata = {
     title: "YOOM",
@@ -12,11 +12,11 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
     return (
         <main className="relative">
-            {/* <Navbar /> */}
-            Navbar
+            <Navbar />
+
             <div className="flex">
-                {/* <Sidebar /> */}
-                Sidebar
+                <Sidebar />
+
                 <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
                     <div className="w-full">{children}</div>
                 </section>
